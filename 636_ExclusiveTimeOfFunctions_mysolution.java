1ef25e528013ec3ba11ee259130e4f9641a65a63
Zhichao Lu <zhichaolu@Zhichaos-MacBook-Pro.local>
2020-12-05 10:12:12 -0500

    636 my solution

---
+++ 636_ExclusiveTimeOfFunctions_mysolution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        
+        int[] output = new int[n];
+        Stack<int[]> stack = new Stack<>();
+        
+        for (String log : logs) {
+            String[] s = log.split(":");
+            int id = Integer.parseInt(s[0]);
+            int timestamp = Integer.parseInt(s[2]);
+
+            if (s[1].equals("start")) {
+                if (!stack.isEmpty()) {
+                    output[stack.peek()[0]] += timestamp - stack.peek()[1];
+                    stack.peek()[1] = timestamp; 
+                }
+                
+                stack.push(new int[] {id, timestamp});
+            } else {
+                int start = stack.pop()[1];
+                output[id] += timestamp - start + 1;
+                
+                if (!stack.isEmpty()) {
+                    stack.peek()[1] = timestamp + 1; 
+                }
+                
+            }
+        }
+        
+        return output;
+        
+    }
+}
