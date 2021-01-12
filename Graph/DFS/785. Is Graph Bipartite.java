8e4435669e9d9fd5f75e3bfaf66f4d64356bb36f
Dan Lu <danlinasyr@gmail.com>
2021-01-11 23:34:48 -0500

    bipartite

---
+++ Graph/DFS/785. Is Graph Bipartite.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] colors = new int[n];
+        int color = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (colors[i] == 0 && !validColor(graph, colors, 1, i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean validColor(int[][] graph, int[] colors, int color, int node) {
+        if (colors[node] != 0) {
+            return colors[node] == color;
+        }
+
+        colors[node] = color;
+
+        for (int next : graph[node]) {
+            if (!validColor(graph, colors, -color, next)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
