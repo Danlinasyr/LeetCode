4c3521ace10f1ed6a69392a8406ae745ee410c2d
Dan Lu <danlinasyr@gmail.com>
2021-01-16 00:08:02 -0500

    787 Djikstra's Algorithm Solution

--- Graph/787. Cheapest Flights Within K Stops.java
+++ Graph/787. Cheapest Flights Within K Stops.java
@@ -1,40 +1,55 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
         Map<Integer, List<int[]>> graph = buildGraph(flights);
-        Set<Integer> visited = new HashSet<>();
+        int[] prices = new int[n];
+        int[] stops = new int[n];
+        Arrays.fill(prices, Integer.MAX_VALUE);
+        Arrays.fill(stops, Integer.MAX_VALUE);
         PriorityQueue<int[]> pq = new PriorityQueue<>((n1, n2) -> (n1[1] - n2[1]));
-        pq.offer(new int[] {src, 0});
-        visited.add(src);
-        while (K >= 0 && !pq.isEmpty()) {
-            int sz = pq.size();
+        pq.offer(new int[] {src, 0, 0});
+        prices[src] = 0;
+        stops[src] = 0;
+        while (!pq.isEmpty()) {
 
-            for (int i = 0; i < sz; i++) {
-                int[] node = pq.poll();
-                int from = node[0];
-                int price = node[1];
+            int[] node = pq.poll();
+            int city = node[0];
+            int price = node[1];
+            int stop = node[2];
 
-
-                for (int[] to : graph.get(from)) {
-
-                    if (to[0] == dst) {
-                        return price + to[1];
-                    }
-
-                    pq.offer(new int[] {to[0], price + to[1]});
-                    visited.add(to[0]);
-                }
+            if (city == dst) {
+                return price;
             }
 
-            K--;
+            if (stop > K) {
+                continue;
+            }
+
+            if (!graph.containsKey(city)) {
+                continue;
+            }
+            for (int[] next : graph.get(city)) {
+
+                if (price + next[1] < prices[next[0]]) {
+                    pq.offer(new int[] {next[0], price + next[1], stop + 1});
+                    prices[next[0]] = price + next[1];
+                }
+
+                else {
+                    pq.offer(new int[] {next[0], price + next[1], stop + 1});
+                    stops[next[0]] = stop + 1;
+                }
+
+
+            }
+
         }
 
-        return -1;
+        return prices[dst] == Integer.MAX_VALUE ? -1 : prices[dst];
 
     }
 
     private Map<Integer, List<int[]>> buildGraph(int[][] flights) {
         Map<Integer, List<int[]>> graph = new HashMap<>();
-
         for (int[] flight : flights) {
             if (!graph.containsKey(flight[0])) {
                 graph.put(flight[0], new ArrayList<int[]>());
